5097e7d9bf96b6a4b31c46e81163f465709f30d8
e <stefanycristinas009@gmail.com>
2026-02-17 15:21:29 -0300

    feat: implementa método verificarPrimo e exibição no console

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Scanner entrada = new Scanner(System.in);
         String parImpar;
         String positivoNegativo;
+        String primo;
 
         System.out.print("Digite um número: ");
         int numero = entrada.nextInt();
@@ -18,12 +19,39 @@ public class Main {
         }
 
         if(numero % 2 != 0){
-            parImpar = "ímpar";
+            parImpar = "Ímpar";
         }else{
             parImpar = "Par";
         }
 
+        if(verificarPrimo(numero)){
+           primo = "Primo";
+        }else {
+            primo = "Não primo";
+        }
+
+        entrada.close();
+        System.out.println("*-------------------------*");
+        System.out.println(" Verificando o número "+numero);
+        System.out.println("*-------------------------*");
+        System.out.println("Positivo/Negativo: "+positivoNegativo);
+        System.out.println("Par/Ímpar: "+parImpar);
+        System.out.println("Primo/Não Primo: "+primo);
+        System.out.println("*-------------------------*");
 
 
+    }
+    //verifica se o numero é primo
+    public static boolean verificarPrimo(int numero){
+        if(numero <= 1){
+            return false;
+        }else{
+            for (int i = 2;i < numero;i++){
+                if (numero % i == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
