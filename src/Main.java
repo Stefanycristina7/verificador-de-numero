a9a88dcb4875e4235cb567914f44bc7cfd46866a
e <stefanycristinas009@gmail.com>
2026-02-17 13:29:14 -0300

    feat: implementa verificação de número positivo, negativo, par e ímpar

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String parImpar;
+        String positivoNegativo;
+
+        System.out.print("Digite um número: ");
+        int numero = entrada.nextInt();
+
+        if(numero < 0){
+            positivoNegativo = "Negativo";
+        } else if (numero > 0) {
+            positivoNegativo = "Positivo";
+        }else {
+            positivoNegativo = "Zero";
+        }
+
+        if(numero % 2 != 0){
+            parImpar = "ímpar";
+        }else{
+            parImpar = "Par";
+        }
+
+
 
     }
 }
