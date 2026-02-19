e4ad408053231f439dd059d31d25a6062c423f5f
e <stefanycristinas009@gmail.com>
2026-02-18 22:40:53 -0300

    refactor: substitui if/else por operador ternário e limita laço usando Math.sqrt()

--- src/Main.java
+++ src/Main.java
@@ -10,44 +10,41 @@ public class Main {
         System.out.print("Digite um número: ");
         int numero = entrada.nextInt();
 
-        if(numero < 0){
+        if (numero < 0) {
             positivoNegativo = "Negativo";
         } else if (numero > 0) {
             positivoNegativo = "Positivo";
-        }else {
+        } else {
             positivoNegativo = "Zero";
         }
 
-        if(numero % 2 != 0){
+        if (numero % 2 != 0) {
             parImpar = "Ímpar";
-        }else{
+        } else {
             parImpar = "Par";
         }
 
-        if(verificarPrimo(numero)){
-           primo = "Primo";
-        }else {
-            primo = "Não primo";
-        }
+        primo = (verificarPrimo(numero)) ? "Primo" : "Não primo";
 
         entrada.close();
         System.out.println("*-------------------------*");
-        System.out.println(" Verificando o número "+numero);
+        System.out.println(" Verificando o número " + numero);
         System.out.println("*-------------------------*");
-        System.out.println("Positivo/Negativo: "+positivoNegativo);
-        System.out.println("Par/Ímpar: "+parImpar);
-        System.out.println("Primo/Não Primo: "+primo);
+        System.out.println("Positivo/Negativo: " + positivoNegativo);
+        System.out.println("Par/Ímpar: " + parImpar);
+        System.out.println("Primo/Não Primo: " + primo);
         System.out.println("*-------------------------*");
 
 
     }
+
     //verifica se o numero é primo
-    public static boolean verificarPrimo(int numero){
-        if(numero <= 1){
+    public static boolean verificarPrimo(int numero) {
+        if (numero <= 1) {
             return false;
-        }else{
-            for (int i = 2;i < numero;i++){
-                if (numero % i == 0){
+        } else {
+            for (int i = 2; i <= Math.sqrt(numero); i++) {
+                if (numero % i == 0) {
                     return false;
                 }
             }
